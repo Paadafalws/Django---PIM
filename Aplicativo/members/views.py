324f8a69bf74cff23a65507e0504eff460ff6d1a
Paadafalws <99057639+Paadafalws@users.noreply.github.com>
2023-03-13 17:13:56 -0300

    AGOORA FOI

--- Aplicativo/members/views.py
+++ Aplicativo/members/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Post
+from django.shortcuts import render
 
 def members(request):
-  template = loader.get_template('myfirst.html')
+  template = loader.get_template('home.html') #AKI QUE COMANDA ONDE VOCE TEM QUE IR PARA ESSA POHA DE SITGE
   return HttpResponse(template.render())
