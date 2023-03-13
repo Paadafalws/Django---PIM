d4835f91fe763370af9729ab03e5dae76be21fbe
Paadafalws <99057639+Paadafalws@users.noreply.github.com>
2023-03-13 17:35:01 -0300

    1

--- Aplicativo/members/views.py
+++ Aplicativo/members/views.py
@@ -6,3 +6,4 @@ from django.shortcuts import render
 def members(request):
   template = loader.get_template('home.html') #AKI QUE COMANDA ONDE VOCE TEM QUE IR PARA ESSA POHA DE SITGE
   return HttpResponse(template.render())
+
